c980bc08055dfbfb942282a092d2c070c4e8aedc
GOWTHAMS03 <86407286+GOWTHAMS03@users.noreply.github.com>
2023-05-16 09:33:48 +0530

    Update webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,23 +7,22 @@ const CURRENT_WORKING_DIR = process.cwd();
 module.exports = {
   entry: [path.join(CURRENT_WORKING_DIR, 'client/app/index.js')],
   resolve: {
-
     extensions: ['.js', '.json', '.css', '.scss', '.html'],
     alias: {
       app: 'client/app'
-    },
-    fallback: {
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      assert: require.resolve('assert/'),
-      stream: require.resolve('stream-browserify'),
-      http: require.resolve('stream-http'),
-      crypto: require.resolve('crypto-browserify'),
-      net: require.resolve("net-browserify"),
-      tls: require.resolve("tls-browserify"),
-      path: require.resolve("path-browserify"),
-      os: require.resolve("os-browserify/browser"),
     }
+  },
+  fallback: {
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    assert: require.resolve('assert/'),
+    stream: require.resolve('stream-browserify'),
+    http: require.resolve('stream-http'),
+    crypto: require.resolve('crypto-browserify'),
+    net: require.resolve("net-browserify"),
+    tls: require.resolve("tls-browserify"),
+    path: require.resolve("path-browserify"),
+    os: require.resolve("os-browserify/browser"),
   },
   module: {
     rules: [
